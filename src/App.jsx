9e84e87e561cdfb5278b9ff3f778856db0ac494b
Matias <matiasbrogliamdp@gmail.com>
2021-02-08 16:21:58 -0300

    ItemCount set

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavbarComponent from './components/navBar';
 import ItemListContainer from './container/ItemListContainer';
+import ItemCount from './components/ItemCount';
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
 
       <NavbarComponent/>
       <ItemListContainer product="Curso de Literatura" productDescription="Curso para profesores de Literatura" price="$15.000" />
+      <ItemCount/>
 
     </>
   );
